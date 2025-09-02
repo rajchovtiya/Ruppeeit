3accb41db5667bb9fcbb4c1823db0eb58c278d42
rajchovatiya <rajchovatiya783@gmail.com>
2025-09-02 13:36:16 +0530

    add code blog

--- src/pages/landingpage/Lifelong.jsx
+++ src/pages/landingpage/Lifelong.jsx
@@ -7,7 +7,7 @@ function Lifelong() {
             <div className=' flex items-center justify-center flex-col'>
                 <h1 className=' text-2xl sm:text-3xl md:text-4xl font-semibold seconfdont text-center'><span >We understand</span> money is </h1>
                 <h1 className=' text-2xl sm:text-3xl md:text-4xl font-semibold seconfdont'>a lifelong journey.</h1>
-                <p className='mt-3 seconfdont font-medium text-[19px] text-[#262626] text-center'>Learning about money is not a one time class, it’ sa continuous journey</p>
+                <p className='mt-3 seconfdont font-medium text-[19px] text-[#262626] text-center'>Learning about money is not a one time class, it’s a continuous journey</p>
             </div>
             <Features />
         </div>
